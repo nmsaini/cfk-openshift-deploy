b0f6bc1197acf75ddaf803bcd731c517dcd90851
N Saini <dd@services.sainihome.net>
2021-12-20 14:17:10 -0500

    initial checkin

---
+++ remove-kafka-deployment.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+namespace=$1
+
+if [ -z $namespace ]
+then
+    echo "Usage: remove.sh <confluent namespace>"
+    exit 1
+fi
+
+oc project $namespace
+
+# remove all components
+oc delete SchemaRegistry.platform.confluent.io schemaregistry
+oc delete ControlCenter.platform.confluent.io controlcenter
+oc delete KsqlDB.platform.confluent.io ksqldb
+oc delete Connect.platform.confluent.io connect
+oc delete Kafka.platform.confluent.io kafka
+oc delete Zookeeper.platform.confluent.io zookeeper
+
+helm delete operator
+
+secrets="zookeeper-listener \
+    kafka-listener \
+    kafka-zookeeper \
+    connect-kafka \
+    sr-kafka \
+    ksql-kafka \
+    connect-listener \
+    ksql-listener \
+    sr-listener \
+    c3-user \
+    c3-connect \
+    c3-ksql \
+    c3-sr \
+    metric-credentials \
+    kafka-tls \
+    connect-tls \
+    replicator-tls \
+    schemaregistry-tls \
+    ksqldb-tls \
+    controlcenter-tls \
+    zookeeper-tls"
+
+for secret in $secrets
+do
+    oc delete secret $secret
+done
+
+rm confluent-$namespace.p12
